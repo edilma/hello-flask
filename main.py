b90a94b1f5a82b9a8766b0211cabceb1df81966c
Edilma Riano <edilmaria@hotmail.com>
2019-09-09 11:39:12 -0400

    Add hello form

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from flask import Flask 
+from flask import Flask, request
 
+form= """ 
+<!doctype html>
+    <html>
+        <body>
+            <form action = "/hello" method ="POST" >
+            <label for ="first_name">First Name</label>
+            <input type="text" name="first_name" />
+            <input type = "submit" />
+
+            </form>
+        </body>
+    </html>
+
+"""
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
+@app.route("/hello", methods =["POST"])
+def hello():
+    first_name = request.form['first_name']
+    return '<h1>Hello, ' + first_name + '</h1>'
 
 app.run()
+
+
